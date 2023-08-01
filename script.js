d5e55282764a79920b3bf4a251ef9029577e36f7
Paranoiddd-hub <135490354+Paranoiddd-hub@users.noreply.github.com>
2023-08-01 20:30:06 +0300

    Finish drawing

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const box = document.getElementById('container');
+let mouseDown = false;
+document.body.onmousedown = (e) => {mouseDown = true};
+document.body.onmouseup = (e) => {mouseDown = false};
 
 function drawDivs(divNumber) {
     let div = 1;
     while (div <= divNumber*divNumber) {
         div++;
         const newDiv = document.createElement('div');
+        newDiv.addEventListener('mousedown', paintDiv);
+        newDiv.addEventListener('mouseover', paintDiv);
         newDiv.style.height= box.clientHeight/divNumber+'px';
         newDiv.style.width= box.clientWidth/divNumber+'px';
         newDiv.classList.add('paintedDivs');
@@ -15,7 +20,19 @@ function drawDivs(divNumber) {
 
 function clearContainer() {
     const allDivs = document.querySelectorAll('.paintedDivs')
-    allDivs.forEach(div => div.remove())
+    llDivs.forEach(div => div.remove())
 }
 
-drawDivs(20);
+function paintDiv (e) {
+   if (e.type = 'mouseover' && mouseDown) {
+        e.target.style.backgroundColor = 'blue';
+   } else return;
+   // e.stopPropagation();
+   
+    console.log(e.type);
+}
+   
+
+
+
+drawDivs(10);
