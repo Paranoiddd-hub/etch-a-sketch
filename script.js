491f04944f96f776546d6fc246fd9f784e67b4d9
Paranoiddd-hub <135490354+Paranoiddd-hub@users.noreply.github.com>
2023-08-07 12:02:36 +0300

    Finish the work

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 const box = document.getElementById('container');
+const clearButton = document.getElementById('clear');
 let mouseDown = false;
-document.body.onmousedown = (e) => {mouseDown = true};
-document.body.onmouseup = (e) => {mouseDown = false};
+const size = document.getElementById('size');
+let color = document.getElementById('color');
+
+document.body.onmouseup = () => {mouseDown = false}
+clearButton.addEventListener('click', clearContainer)
+size.onfocus = () => {size.value = ""}
+size.oninput = () => {
+    clearContainer();
+    drawDivs(size.value);
+}
+//size.onblur = () => {size.value = 10}
+
+drawDivs(size.value);
 
 function drawDivs(divNumber) {
     let div = 1;
     while (div <= divNumber*divNumber) {
-        div++;
         const newDiv = document.createElement('div');
+
         newDiv.addEventListener('mousedown', paintDiv);
         newDiv.addEventListener('mouseover', paintDiv);
+
         newDiv.style.height= box.clientHeight/divNumber+'px';
         newDiv.style.width= box.clientWidth/divNumber+'px';
         newDiv.classList.add('paintedDivs');
+
         box.append(newDiv);
-      // console.log(newDiv.clientWidth);
+
+        div++;
     }
 }
 
 function clearContainer() {
-    const allDivs = document.querySelectorAll('.paintedDivs')
-    llDivs.forEach(div => div.remove())
+    const allDivs = document.querySelectorAll('.paintedDivs');
+    allDivs.forEach(div => div.remove());
+    drawDivs(size.value);
 }
 
 function paintDiv (e) {
-   if (e.type = 'mouseover' && mouseDown) {
-        e.target.style.backgroundColor = 'blue';
-   } else return;
-   // e.stopPropagation();
-   
-    console.log(e.type);
+    if (e.type === 'mousedown') {
+        mouseDown = true;
+    }
+
+    if (mouseDown && (e.type === 'mouseover') || mouseDown) {
+        e.target.style.backgroundColor = color.value;
+    } else return
 }
-   
-
-
-
-drawDivs(10);
